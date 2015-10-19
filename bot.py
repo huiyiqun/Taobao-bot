c471022ea8c16fbd558db1c2aecdeea2a8c21706
Hui Yiqun <huiyiqun@gmail.com>
2015-10-19 23:12:56 +0800

    bind TB_Searcher to telegram bot api

---
+++ bot.py
@@ -0,0 +1,51 @@
+import telebot
+import logging
+from tbapi import TB_Searcher
+
+logging.basicConfig(level=logging.DEBUG)
+telebot.logger.setLevel(logging.DEBUG)
+logger = logging.getLogger('tbbot')
+
+
+def readfile(filename):
+    with open(filename, 'r') as f:
+        return f.read().strip()
+
+bot = telebot.TeleBot(readfile('token.txt'))
+
+
+def retrieve_arg(text):
+    return text.split(' ', 1).pop()
+
+
+@bot.message_handler(commands=['tbsearch'])
+def search_handler(message):
+    searcher = TB_Searcher(retrieve_arg(message.text))
+    try:
+        bot.reply_to(
+            message,
+            '\n'.join(
+                '{}: {}'.format(title, url)
+                for title, url in searcher.list_items())
+        )
+    except:
+        logger.exception('Fail to list items')
+        bot.reply_to(message, 'Ooops, 臣妾做不到啊')
+
+
+@bot.message_handler(commands=['tbprice'])
+def price_handler(message):
+    searcher = TB_Searcher(retrieve_arg(message.text))
+    try:
+        bot.reply_to(
+            message,
+            '最低价：{:.2f}RMB\n最高价：{:.2f}RMB\n平均价：{:.2f}RMB'
+            .format(*searcher.price_tuple())
+        )
+    except:
+        logger.exception('Fail to print prices')
+        bot.reply_to(message, 'Ooops, 臣妾做不到啊')
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
